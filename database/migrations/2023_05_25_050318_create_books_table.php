a0a307134a3f64819f433d3f2f8f956c37ac795f
Nour AL-Borno <nour.alborno98@gmail.com>
2023-05-29 11:56:27 +0300

    first commit

---
+++ database/migrations/2023_05_25_050318_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title");
+            $table->string("author");
+            $table->string("year");
+            $table->text("book");
+            $table->text("details");
+            $table->string('book_img')->nullable();
+            $table->decimal('price');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ database/migrations/2023_05_25_050951_create_soldbooks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoldbooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soldbooks', function (Blueprint $table) {
+
+            $table->timestamps();
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->integer("sold")->nullable();
+
+            $table->primary(['user_id', 'book_id']);  
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soldbooks');
+    }
+}
